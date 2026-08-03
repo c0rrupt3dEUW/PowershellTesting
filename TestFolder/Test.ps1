4ad310aa9a5f400434c2c27d82b9a66773b9d6ea
Michael Neill <michael.neill.mn@googlemail.com>
2026-08-03 14:28:33 +0100

    adding test script

---
+++ TestFolder/Test.ps1
@@ -0,0 +1,8 @@
+﻿# Get the directory where the script is located
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Path to the output file
+$filePath = Join-Path $scriptDir "test.txt"
+
+# Create the file and write the text
+"test text" | Set-Content -Path $filePath
